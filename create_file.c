5fa532d678541fc07a13b92f41c5919ab6106eaf
gilbertrec <g.recupito@studenti.unisa.it>
2021-07-06 02:54:03 +0200

    Modified for Makefile and test execution

--- create_file.c
+++ create_file.c
@@ -3,8 +3,24 @@
 
 
 int main (int argc, char** argv){
-    FILE* fp=fopen("strong_results.csv","w");
+    FILE* fp=fopen("strong_results_little.csv","w");
     fprintf(fp,"NP,N,M,G,Time\n");
-    FILE* fp_weak=fopen("weak_results.csv","w");
+    FILE* fp_weak=fopen("weak_results_little.csv","w");
     fprintf(fp_weak,"NP,N,M,G,Time\n");
+    fclose(fp);
+    fclose(fp_weak);
+
+    fp=fopen("strong_results_medium.csv","w");
+    fprintf(fp,"NP,N,M,G,Time\n");
+    fp_weak=fopen("weak_results_medium.csv","w");
+    fprintf(fp_weak,"NP,N,M,G,Time\n");
+    fclose(fp);
+    fclose(fp_weak);
+
+    fp=fopen("strong_results_big.csv","w");
+    fprintf(fp,"NP,N,M,G,Time\n");
+    fp_weak=fopen("weak_results_big.csv","w");
+    fprintf(fp_weak,"NP,N,M,G,Time\n");
+    fclose(fp);
+    fclose(fp_weak);
 }
